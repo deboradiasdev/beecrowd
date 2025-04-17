b8c696dddcded2d6a7544012f30d9c6414a649ea
deboradiasdev <contatodeboradev@gmail.com>
2025-04-16 21:19:23 -0300

    atualizando 1024 - criptografia

--- 1024.py
+++ 1024.py
@@ -1,30 +1,51 @@
-mensagem = input()
-msg_criptografada = ""
+class Criptografia:
+    def __init__(self, mensagem):
+        self.mensagem = mensagem
 
-#Verifica caractere e acrescenta 3 posições a direita na tabela ASCII
-for caractere in mensagem:
-    if caractere.isalpha():
-        msg_criptografada += chr(ord(caractere) + 3)
-    else:
-        msg_criptografada += caractere
+    def passo1(self, texto):
+    # Verifica caractere e desloca letras3 posições a direita na tabela ASCII
+        resultado = ""
+        for caractere in texto:
+            if caractere.isalpha():
+                resultado += chr(ord(caractere) + 3)
+            else:
+                resultado += caractere
+        return resultado
+    
+    def passo2(self, texto):
+        # Inverte a string
+        return texto[::-1]
 
-#Inverte a string
-msg_criptografada = msg_criptografada[::-1]
+    def passo3(self, texto):
+        # Parte a string na metade e desloca essa metade para a esquerda
+        metade = len(texto) // 2
+        primeira_metade = texto[:metade]
 
-#Parte a string na metade e desloca essa metade para a esquerda
-metade = len(msg_criptografada) // 2
-primeira_metade = msg_criptografada[:metade]
+        segunda_metade = []
+        for caractere in texto[metade:]:
+            if caractere.isalpha():
+                novo_caractere = chr(ord(caractere)-1)
+                segunda_metade.append(novo_caractere)
+            else:
+                segunda_metade.append(caractere)
 
-segunda_metade = []
-for caractere in msg_criptografada[metade:]:
-    if caractere.isalpha():
-        novo_caractere = chr(ord(caractere)-1)
-        segunda_metade.append(novo_caractere)
-    else:
-        segunda_metade.append(caractere)
+        segunda_metade = ''.join(segunda_metade)
+        return primeira_metade + segunda_metade
 
-segunda_metade = ''.join(segunda_metade)
+    def criptografar(self):
+    # Executa todos os passos da classe
 
-msg_criptografada = primeira_metade + segunda_metade
+        etapa1 = self.passo1(self.mensagem)
+        etapa2 = self.passo2(etapa1)
+        final = self.passo3(etapa2)
 
-print(msg_criptografada)
+        return final
+
+# Ler o número de mensagens
+qtd = int(input())
+
+for _ in range(qtd):
+    mensagem = input()
+    criptografia = Criptografia(mensagem)
+    resultado = criptografia.criptografar()
+    print(resultado)
